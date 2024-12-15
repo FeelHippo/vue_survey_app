6932d3b8fd4b45fd67d8e8ac3fcb842443d74d9d
FeelHippo <mezzasegolas@gmail.com>
2024-12-16 00:44:30 +0100

    type errors

--- src/store/mutations/mutations.instance.ts
+++ src/store/mutations/mutations.instance.ts
@@ -1,12 +1,12 @@
 import type { SurveyMutationBase } from './mutations.types'
 import type { SurveyState } from '../store.types'
-import type { QuestionInterface } from '../../entity/answer.types'
+import type { AnswerData } from '../../entity/answer.types'
 
 export const mutations: SurveyMutationBase = {
-  fetchSurveyAnswers(state: SurveyState, payload: QuestionInterface[]): void {
+  fetchSurveyAnswers(state: SurveyState, payload: AnswerData[]): void {
     payload.map((answer) => state.answers.push(answer))
   },
-  createAnswer(state: SurveyState, payload: QuestionInterface): void {
+  createAnswer(state: SurveyState, payload: AnswerData): void {
     state.questions.push(payload)
   },
 }
